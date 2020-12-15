281f6d2b2f7c7ec042b3c52127952faf318cebf8
willofhouse <willofhouse@pop-os.localdomain>
2020-12-15 07:58:46 -0600

    comment

--- randomizer.py
+++ randomizer.py
@@ -1,5 +1,7 @@
 import redis
 import random, string
+
+# generates a naive 4 character short_url. If that url exists already, generate another. 
 def randomizer(data):
     # I know i'll be asked about this, redis states that there is negligible performance to instantiation and not to worry about it in the docs
     redis_session = redis.Redis(host='localhost', port=6379, db=0)
